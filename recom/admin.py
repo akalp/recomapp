1724b8cfc0d2743897e8358d206c530307cb9803
Hasan Akalp <hasan.akalp@gmail.com>
2020-04-11 17:17:39 +0300

    ADD modelt to admin.site

--- recom/admin.py
+++ recom/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from recom.models import User, PieceBaseModel, Music, MusicGenre, Movie, MovieGenre, Book, BookGenre, Point, Comment, Author, Director, Performer, Singer, Album
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(PieceBaseModel)
+admin.site.register(Music)
+admin.site.register(MusicGenre)
+admin.site.register(Movie)
+admin.site.register(MovieGenre)
+admin.site.register(Book)
+admin.site.register(BookGenre)
+admin.site.register(Point)
+admin.site.register(Comment)
+admin.site.register(Author)
+admin.site.register(Director)
+admin.site.register(Performer)
+admin.site.register(Singer)
+admin.site.register(Album)
