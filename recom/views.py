64cbb03b5f260c983e71347e48e1d6375ffc5f23
Hasan Akalp <hasan.akalp@gmail.com>
2020-04-11 17:18:00 +0300

    ADD default director photo

--- recom/views.py
+++ recom/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic
+
+from recom.models import Movie
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'recom/index.html'
+
+
+class MovieListView(generic.ListView):
+    model = Movie
+    template_name = 'recom/movie_list.html'
+    context_object_name = "movies"
+
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    context_object_name = 'movie'
+    template_name = 'recom/movie_detail.html'
+
+
+class MovieDeleteView(generic.DeleteView):
+    model = Movie
+    success_url = reverse_lazy('recom:movie_list')
