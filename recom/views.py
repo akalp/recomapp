4057b292e2e51692b8534042843116bfe0a21991
Hasan Akalp <hasan.akalp@gmail.com>
2020-04-25 02:05:07 +0300

    ADD Best, Trend Movies to movies index and CREATE special views for them

--- recom/views.py
+++ recom/views.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.db.models import Avg, Count
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,10 +14,51 @@ class IndexView(generic.TemplateView):
     template_name = 'recom/index.html'
 
 
-class MovieListView(generic.ListView):
+class MovieBestListView(generic.ListView):
     model = Movie
     template_name = 'recom/movie_list.html'
     context_object_name = "movies"
+
+    def get_queryset(self):
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data["page_title"] = "Best Movies"
+        return data
+
+
+class MovieTrendListView(generic.ListView):
+    model = Movie
+    template_name = 'recom/movie_list.html'
+    context_object_name = "movies"
+
+    def get_queryset(self):
+        return self.model.objects.filter(points__date__gte=datetime.date.today() - datetime.timedelta(days=7)).annotate(
+            counts=Count('points'), avg_point=(Avg('points__point'))).order_by('-counts')
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data["page_title"] = "Trend Movies"
+        return data
+
+
+class MovieIndex(generic.ListView):
+    model = Movie
+    template_name = 'recom/movie_index.html'
+    context_object_name = "movies"
+
+    def get_queryset(self):
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')[:11]
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data["trends"] = self.model.objects.filter(
+            points__date__gte=datetime.date.today() - datetime.timedelta(days=7)).annotate(counts=Count('points'),
+                                                                                           avg_point=(Avg(
+                                                                                               'points__point'))).order_by(
+            '-counts')[:11]  ## Son 7 güne ait puan ortalamasını getiriyor
+        return data
 
 
 class MovieDetailView(generic.DetailView):
