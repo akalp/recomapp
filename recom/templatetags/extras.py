a57967c71878c63b0f0c1f507793b83402a103d2
Hasan Akalp <hasan.akalp@gmail.com>
2020-05-02 16:59:19 +0300

    ADD filters for checking follow and wish situations

--- recom/templatetags/extras.py
+++ recom/templatetags/extras.py
@@ -1,7 +1,7 @@
 from django import template
 from django.urls import reverse
 
-from recom.models import Movie, Book, Music
+from recom.models import Movie, Book, Music, PieceBaseModel
 
 register = template.Library()
 
@@ -10,3 +10,13 @@ def get_url(pk):
     for model in [Movie, Book, Music]:
         if model.objects.filter(pk=pk).exists():
             return reverse("recom:{}_detail".format(model.__name__.lower()), args=(pk,))
+
+
+@register.filter
+def is_wished(pk, user):
+    return user.wishes.objects.filter(pk=pk).exists()
+
+
+@register.filter
+def is_followed(pk, user):
+    return user.follows.objects.filter(pk=pk).exists()
