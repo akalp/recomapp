3f40e3b9680ddeb9ac83eeb6a9e906070bcdbf62
Hasan Akalp <hasan.akalp@gmail.com>
2020-05-09 15:59:22 +0300

    REFACTOR change order of views and make groups

--- recom/views.py
+++ recom/views.py
@@ -37,6 +37,33 @@ class IndexView(generic.TemplateView):
         return data
 
 
+## MOVIE
+class MovieIndex(generic.ListView):
+    model = Movie
+    template_name = 'recom/movie_index.html'
+    context_object_name = "movies"
+
+    def get_queryset(self):
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))[:11]
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+
+        data["bests"] = self.model.objects.filter(points__isnull=False).annotate(
+            avg_point=(Avg('points__point'))).order_by('-avg_point')[:11]
+
+        trending_time = datetime.date.today() - datetime.timedelta(days=7)
+        data["trends"] = self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
+            Case(When(points__date__gte=trending_time, then=1),
+                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by(
+            '-counts')[:11]
+
+        act_user_query = "select u.*, count(p.id) as count from recom_{} m, recom_piecebasemodel b, recom_point p, recom_user u where m.piecebasemodel_ptr_id = b.id and p.piece_id=b.id and p.user_id=u.id group by u.id order by count desc".format(
+            "movie")
+        data["active_users"] = get_user_model().objects.raw(act_user_query)[:11]
+        return data
+
+
 class MovieListView(generic.ListView):
     model = Movie
     template_name = 'recom/movie_list.html'
@@ -83,32 +110,6 @@ class MovieTrendListView(generic.ListView):
         return data
 
 
-class MovieIndex(generic.ListView):
-    model = Movie
-    template_name = 'recom/movie_index.html'
-    context_object_name = "movies"
-
-    def get_queryset(self):
-        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))[:11]
-
-    def get_context_data(self, *args, **kwargs):
-        data = super().get_context_data(*args, **kwargs)
-
-        data["bests"] = self.model.objects.filter(points__isnull=False).annotate(
-            avg_point=(Avg('points__point'))).order_by('-avg_point')[:11]
-
-        trending_time = datetime.date.today() - datetime.timedelta(days=7)
-        data["trends"] = self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
-            Case(When(points__date__gte=trending_time, then=1),
-                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by(
-            '-counts')[:11]
-
-        act_user_query = "select u.*, count(p.id) as count from recom_{} m, recom_piecebasemodel b, recom_point p, recom_user u where m.piecebasemodel_ptr_id = b.id and p.piece_id=b.id and p.user_id=u.id group by u.id order by count desc".format(
-            "movie")
-        data["active_users"] = get_user_model().objects.raw(act_user_query)[:11]
-        return data
-
-
 class MovieDetailView(generic.DetailView):
     model = Movie
     template_name = 'recom/movie_detail.html'
@@ -120,31 +121,28 @@ class MovieDetailView(generic.DetailView):
         return data
 
 
-class BookListView(generic.ListView):
-    model = Book
-    template_name = 'recom/book_list.html'
-    context_object_name = "books"
+## MUSIC
+class MusicIndex(generic.ListView):
+    model = Music
+    template_name = 'recom/music_index.html'
+    context_object_name = "music"
 
     def get_queryset(self):
-        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))[:11]
 
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
-        data["page_title"] = "Books"
-        return data
+        data["bests"] = self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')[:11]
+        trending_time = datetime.date.today() - datetime.timedelta(days=7)
+        data["trends"] = self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
+            Case(When(points__date__gte=trending_time, then=1),
 
+                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by('-counts')[:11]
 
-class BookBestListView(generic.ListView):
-    model = Book
-    template_name = 'recom/book_list.html'
-    context_object_name = "books"
+        act_user_query = "select u.*, count(p.id) as count from recom_{} m, recom_piecebasemodel b, recom_point p, recom_user u where m.piecebasemodel_ptr_id = b.id and p.piece_id=b.id and p.user_id=u.id group by u.id order by count desc".format(
+            "music")
 
-    def get_queryset(self):
-        return self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')
-
-    def get_context_data(self, *args, **kwargs):
-        data = super().get_context_data(*args, **kwargs)
-        data["page_title"] = "Best Books"
+        data["active_users"] = get_user_model().objects.raw(act_user_query)
         return data
 
 
@@ -176,23 +174,6 @@ class MusicBestListView(generic.ListView):
         return data
 
 
-class BookTrendListView(generic.ListView):
-    model = Book
-    template_name = 'recom/book_list.html'
-    context_object_name = "books"
-
-    def get_queryset(self):
-        trending_time = datetime.date.today() - datetime.timedelta(days=7)
-        return self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
-            Case(When(points__date__gte=trending_time, then=1),
-                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by('-counts')
-
-    def get_context_data(self, *args, **kwargs):
-        data = super().get_context_data(*args, **kwargs)
-        data["page_title"] = "Trend Books"
-        return data
-
-
 class MusicTrendListView(generic.ListView):
     model = Music
     template_name = 'recom/music_list.html'
@@ -210,6 +191,18 @@ class MusicTrendListView(generic.ListView):
         return data
 
 
+class MusicDetailView(generic.DetailView):
+    model = Music
+    template_name = 'recom/music_detail.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data["full_pointers"] = get_user_model().objects.filter(
+            Q(points__point=5) & Q(points__piece_id=kwargs['object']))
+        return data
+
+
+## BOOK
 class BookIndex(generic.ListView):
     model = Book
     template_name = 'recom/book_index.html'
@@ -233,27 +226,48 @@ class BookIndex(generic.ListView):
         return data
 
 
-class MusicIndex(generic.ListView):
-    model = Music
-    template_name = 'recom/music_index.html'
-    context_object_name = "music"
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'recom/book_list.html'
+    context_object_name = "books"
 
     def get_queryset(self):
-        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))[:11]
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point')))
 
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
-        data["bests"] = self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')[:11]
+        data["page_title"] = "Books"
+        return data
+
+
+class BookBestListView(generic.ListView):
+    model = Book
+    template_name = 'recom/book_list.html'
+    context_object_name = "books"
+
+    def get_queryset(self):
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point'))).order_by('-avg_point')
+
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data["page_title"] = "Best Books"
+        return data
+
+
+class BookTrendListView(generic.ListView):
+    model = Book
+    template_name = 'recom/book_list.html'
+    context_object_name = "books"
+
+    def get_queryset(self):
         trending_time = datetime.date.today() - datetime.timedelta(days=7)
-        data["trends"] = self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
+        return self.model.objects.all().annotate(avg_point=(Avg('points__point')), counts=Count(
             Case(When(points__date__gte=trending_time, then=1),
+                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by('-counts')
 
-                 output_field=IntegerField()))).filter(points__date__gte=trending_time).order_by('-counts')[:11]
-
-        act_user_query = "select u.*, count(p.id) as count from recom_{} m, recom_piecebasemodel b, recom_point p, recom_user u where m.piecebasemodel_ptr_id = b.id and p.piece_id=b.id and p.user_id=u.id group by u.id order by count desc".format(
-            "music")
-
-        data["active_users"] = get_user_model().objects.raw(act_user_query)
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        data["page_title"] = "Trend Books"
         return data
 
 
@@ -268,17 +282,7 @@ class BookDetailView(generic.DetailView):
         return data
 
 
-class MusicDetailView(generic.DetailView):
-    model = Music
-    template_name = 'recom/music_detail.html'
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        data["full_pointers"] = get_user_model().objects.filter(
-            Q(points__point=5) & Q(points__piece_id=kwargs['object']))
-        return data
-
-
+## USER
 class UserDetailView(generic.DetailView):
     model = get_user_model()
     context_object_name = 'user_profile'
@@ -334,6 +338,7 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('recom:index'))
 
 
+## Operations
 @login_required
 def wish(request, pk):
     piece = PieceBaseModel.objects.get(pk=pk)
